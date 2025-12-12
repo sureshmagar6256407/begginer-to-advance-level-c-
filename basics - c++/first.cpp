635b03a5aa627c560a0b2e3aba946e9d3b6b4290
Suresh Pun <sureshmagar6256406@gmail.com>
2025-12-12 12:10:34 +0545

    number

---
+++ basics - c++/first.cpp
@@ -0,0 +1,27 @@
+// #include <iostream> 
+// using namespace std;
+
+// int main() {
+//     cout << 1 + 1;
+// }
+
+
+// # include <iostream> 
+// using namespace std;
+// int main() {
+//     cout << "suresh pun magar" ;
+// }
+
+
+// #include <iostream>
+// using namespace std ;
+// int main () {
+//     cout <<"suresh pun magar" <<endl ;#endl means new line
+//     cout <<"tekam oli" ;
+// }
+
+#include <iostream>
+using namespace std ;
+int main() {
+    cout << "suresh pun magar \n"; //its also gives new line 
+}
---
+++ basics - c++/number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "4" << endl;
+    cout << 4 << endl;
+    cout << 4 + 3 << "\n";
+    cout << "4*3 ";
+}
