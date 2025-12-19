a955d0ba689575a0ecdb86486ebdc3b112dba551
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2025-12-19 06:48:43 +0545

    update

--- basics - c++/first.cpp
+++ basics - c++/first.cpp
@@ -48,18 +48,35 @@
     
 // }
 
-// write a program in c++ to print all the even number lesst than20
-#include <iostream>
-using namespace std;
+// // write a program in c++ to print all the even number lesst than20
+// #include <iostream>
+// using namespace std;
 
-int main () {
-   int  i  = 1 ;
-   do
-   {
-    if (i % 2 ==0) {
-        cout <<i <<endl;
-    }
-    i++;
-   }while (i <= 20);
+// int main () {
+//    int  i  = 1 ;
+//    do
+//    {
+//     if (i % 2 ==0) {
+//         cout <<i <<endl;
+//     }
+//     i++;
+//    }while (i <= 20);
    
+// }
+
+// print all odd  number less than 20
+#include <iostream>
+using namespace std; 
+
+int main () { 
+    int i  = 1 ;
+    while (i<= 20)
+    {
+        if (i % 2 != 0) {
+            
+            cout << i <<endl;
+        }
+        i++ ;
+    }
+    
 }
